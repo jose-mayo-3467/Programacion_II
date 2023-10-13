b0582cb7b1d8fc189aa14d12e7482d485dcf7a1b
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2023-10-13 07:32:05 -0600

    Update and rename poke to pokemon.java

---
+++ Pokemon_Abstraccion/Pokemon/pokemon.java
@@ -0,0 +1,48 @@
+public class Pokemon {
+    
+    private int hp;
+    private int nivel;
+    private String nombre;
+    private String tipo;
+
+    public Pokemon(int nivel, String nombre, String tipo) {
+        this.nivel = nivel;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        int hp = 100;
+    }
+
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    private void calculaDanio(int danio) {
+        this.hp -= danio;
+            System.out.printf("%s recibe %d puntos de danio\n",
+            this.getNombre(), danio);
+    }
+    public void recibirAtaque(String movimiento) {
+        System.out.printf("%s recibe ATK %s\n", this.getNombre(), movimiento);
+        calculaDanio((int)( Math.random() * 10 + 1));
+    }
+    public void atacar(String movimiento, Pokemon pokemon) {
+        System.out.printf("%s ataca a %s con %s\n", this.getNombre(),
+        pokemon.getNombre(), movimiento);
+        pokemon.recibirAtaque(movimiento);
+        }
+    public static void main(String[] args) {
+    
+    }
+}
