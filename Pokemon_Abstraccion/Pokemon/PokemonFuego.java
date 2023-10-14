471d32dbc5649923e90515b1a6c91071bb723a7f
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2023-10-13 22:58:24 -0600

    Update PokemonFuego.java

--- Pokemon_Abstraccion/Pokemon/PokemonFuego.java
+++ Pokemon_Abstraccion/Pokemon/PokemonFuego.java
@@ -1,7 +1,31 @@
 package pokemon_herencia;
 
 public class PokemonFuego extends Pokemon {
-    public PokemonFuego (int nivel, String nombre, String tipo ) {
-    super(nivel ,nombre, "Dragon");
+    public PokemonFuego (int nivel, String nombre, Tipo tipo ) {
+    super(nivel ,nombre, Tipo.DRAGON);
+   
+    ListaMovimientos listaMovimientos = new ListaMovimientos();
+     
+    setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Colmillo ígneo"));
+    setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Infierno"));
+    setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Fuego sagrado"));
+    setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Látigo ígneo"));   
+     
+    @Override
+    public double obtenerEfectividad(Pokemon pokemon){
+        double efectividad= 1.0;
+        
+        if(pokemon.getTipo()== Tipo.HADA) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.FUEGO) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.HIELO) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.PLANTA) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.TIERRA) efectividad = 2.0;
+        if(pokemon.getTipo()== Tipo.AGUA) efectividad = 2.0;
+        if(pokemon.getTipo()== Tipo.ACERO) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.BICHO) efectividad = 0.5;
+        if(pokemon.getTipo()== Tipo.ROCA) efectividad = 2.0;
+
+        return efectividad;
     }
+
 }
