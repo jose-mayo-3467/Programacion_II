489ee0df146ff24a09bc2e3d4bcd2d21555e53ab
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2023-10-27 20:48:24 -0600

    Create tipo.java

---
+++ pokemon_polimorfismo/pokemon/tipo.java
@@ -0,0 +1,15 @@
+package pokemon_polimorfismo;
+
+/**
+ *
+ * @author josem
+ */
+public enum tipo {
+    
+    ACERO, AGUA, BICHO, DRAGON,
+    ELECTRICO, FANTASMA, FUEGO, HADA,
+    HIELO, LUCHA, NORMAL, PLANTA,
+    PSIQUICO, ROCA, SINIESTRO, TIERRA,
+    VENENO, VOLADOR
+    
+}
