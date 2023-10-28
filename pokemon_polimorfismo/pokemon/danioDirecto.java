6e302c481b3be21d0dee059bea99ab4c7e1e52bf
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2023-10-27 20:50:51 -0600

    Create danioDirecto.java

---
+++ pokemon_polimorfismo/pokemon/danioDirecto.java
@@ -0,0 +1,19 @@
+package pokemon_polimorfismo;
+
+/**
+ *
+ * @author josem
+ */
+public class danioDirecto {
+    private double bonificacion;
+    private double efectividad;
+    private int variacion;
+    private int lvl;
+    private int cantidadDeAtaqueEspecial;
+    private int poderDelAtaque;
+    private int cantidadDeDefensa;
+    
+    public class calculaElDanioDirecto{
+    
+    }
+}
