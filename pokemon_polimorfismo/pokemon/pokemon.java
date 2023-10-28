c99399d37a32ce451c9e1fb85f8534bcef38ef5a
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2023-10-27 20:47:32 -0600

    Update pokemon.java

--- pokemon_polimorfismo/pokemon/pokemon.java
+++ pokemon_polimorfismo/pokemon/pokemon.java
@@ -1,15 +1,31 @@
-package pokemon_herencia;
-import pokemon.pokemonDragon;
-import pokemon.pokemonFuego;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon_polimorfismo;
 
-public class BatallaPokemon{
-    
-     public static void main(String[] args) {
-         Pokemon Haxorus = new Pokemon(10 ,"Haxorus", "Dragon");
-         Pokemon Charizard = new Pokemon(10 ,"Charizard", "Fuego");
-         Haxorus.atacar("gillotina", Charizard);
-         Charizard.atacar("lanzallamas" ,Haxorus);   
-         
-         
-    }   
+/**
+ *
+ * @author josem
+ */
+public abstract class pokemon {
+    private String nombre;
+    private int hp = 250;
+    private int nivel;
+    private int ataque;
+    private int defensa;
+    private int ataqueEspecial;
+    private int defensaEspecial;
+    private int velocidad;
+    private Movimiento movimiento[];
+
+    tipo gettipo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean getTipo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
