a8ad5998183867b188381cdf58edce765de302d2
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2023-10-27 20:45:44 -0600

    Update charizard.java

--- pokemon_polimorfismo/pokemon/charizard.java
+++ pokemon_polimorfismo/pokemon/charizard.java
@@ -1,1 +1,10 @@
+package pokemon_polimorfismo;
 
+public class charizard extends pokemon {
+    tipo FUEGO;
+    private tipo VOLADOR;
+    
+    public charizard (String nombre, int lvl){
+    }
+ 
+}
